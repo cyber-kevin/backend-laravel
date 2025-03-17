1628da1a2c15897c6738d8def84b0ecaf6c4a8c4
Kevin <devkevferreira@gmail.com>
2025-03-17 04:42:03 -0300

    ok

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('tasks', TaskController::class);
+Route::apiResource('tasks', TaskController::class)->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
